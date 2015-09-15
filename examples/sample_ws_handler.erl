bffa1fbdb93124ba5ccbab08fa090460630133e1
lanastasov <lubomir.anastasov@gmail.com>
2015-09-16 02:36:06 +0300

    fix typo

--- examples/sample_ws_handler.erl
+++ examples/sample_ws_handler.erl
@@ -42,6 +42,6 @@ websocket_info(start, _ConnState, State) ->
     {reply, {text, <<"erlang message received">>}, State}.
 
 websocket_terminate(Reason, _ConnState, State) ->
-    io:format("Websocket closed in state ~p wih reason ~p~n",
+    io:format("Websocket closed in state ~p with reason ~p~n",
               [State, Reason]),
     ok.
